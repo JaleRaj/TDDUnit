03120777d130de2c263d2b1aeac6b15adf097c2e
JaleRajah <72014692+JaleRajah@users.noreply.github.com>
2024-03-25 21:02:47 +0100

    die Klasse isValidEmail erstellt (1.Testmethode erfolgreich)

---
+++ src/main/java/com/example/demo/demo/EmailValidator.java
@@ -0,0 +1,18 @@
+package com.example.demo.demo;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+
+    // Ein einfacher Anfang: Diese Version des regulären Ausdrucks überprüft nicht das Vorhandensein eines "@"
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*" + 
+            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+    );
+
+    public static boolean isValidEmail(String email) {
+        if (email == null) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+}
