3a0a10e810e4f556b73dc59095bfebea31284285
JaleRajah <72014692+JaleRajah@users.noreply.github.com>
2024-03-25 21:16:16 +0100

    Anpassung der Klasse (2.Testmethode erfolgreich)

--- src/main/java/com/example/demo/demo/EmailValidator.java
+++ src/main/java/com/example/demo/demo/EmailValidator.java
@@ -3,9 +3,8 @@ import java.util.regex.Pattern;
 
 public class EmailValidator {
 
-    // Ein einfacher Anfang: Diese Version des regulären Ausdrucks überprüft nicht das Vorhandensein eines "@"
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
-            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*" + 
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" + 
             "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
     );
 
