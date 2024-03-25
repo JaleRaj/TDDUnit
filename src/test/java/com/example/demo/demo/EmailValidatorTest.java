1b19e8fc3ef38c8ca772f697e285e9e305635ee9
JaleRajah <72014692+JaleRajah@users.noreply.github.com>
2024-03-25 21:21:31 +0100

    Testmethode: testEmailValidationWithMultipleAtSymbols() erstellt

--- src/test/java/com/example/demo/demo/EmailValidatorTest.java
+++ src/test/java/com/example/demo/demo/EmailValidatorTest.java
@@ -15,13 +15,21 @@ public class EmailValidatorTest {
                    "Die E-Mail-Adresse sollte als gültig anerkannt werden.");
     }
 
-    
+
     @Test
     public void testEmailValidationWithMissingAtSymbol() {
         // Eine E-Mail-Adresse ohne "@"-Zeichen sollte als ungültig erkannt werden.
         String emailWithoutAt = "testexample.com";
         assertFalse(EmailValidator.isValidEmail(emailWithoutAt),
                     "Die E-Mail-Adresse ohne '@' sollte als ungültig erkannt werden.");
+    }
+
+    @Test
+    public void testEmailValidationWithMultipleAtSymbols() {
+        // Eine E-Mail-Adresse mit mehreren "@"-Zeichen sollte als ungültig erkannt werden.
+        String invalidEmail = "test@@example.com";
+        assertFalse(EmailValidator.isValidEmail(invalidEmail),
+                    "Die E-Mail-Adresse mit mehreren '@' sollte als ungültig erkannt werden.");
     }
     
 }
